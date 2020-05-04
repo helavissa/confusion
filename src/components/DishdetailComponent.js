34c4208c301c2d61232e3d3ee55e2d281edd8390
Dina Bektemirova <helavissa@gmail.com>
2020-05-04 22:57:01 +0200

    presentational and container components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component{
-	
-	constructor(props){
-		super(props);
-	}
 
 	renderComments(comments){
 
@@ -23,10 +19,9 @@ class DishDetail extends Component{
 								--{comment.author},&nbsp; 
 								{new Intl.DateTimeFormat('en-US', {
 									year: 'numeric',
-									month: 'long',
+									month: 'short',
 									day: '2-digit'
 								}).format(new Date(comment.date))}
-
 							</CardText>
 						</div>
 					);
@@ -50,20 +45,22 @@ class DishDetail extends Component{
 		if(this.props.dish != null){
 
 			return(
-				<div className="row">
-					<div className="col-12 col-md-5 m-1">
-						<Card>
-							<CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
-							<CardBody>
-								<CardTitle>{this.props.dish.name}</CardTitle>
-								<CardText>{this.props.dish.description}</CardText>
-							</CardBody>
-						</Card>
-					</div>
-					<div className="col-12 col-md-5 m-1">
-						
-						{this.renderComments(this.props.dish.comments)}
-								
+				<div className="container">
+					<div className="row">
+						<div className="col-12 col-md-5 m-1">
+							<Card>
+								<CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+								<CardBody>
+									<CardTitle>{this.props.dish.name}</CardTitle>
+									<CardText>{this.props.dish.description}</CardText>
+								</CardBody>
+							</Card>
+						</div>
+						<div className="col-12 col-md-5 m-1">
+							
+							{this.renderComments(this.props.dish.comments)}
+									
+						</div>
 					</div>
 				</div>
 			);

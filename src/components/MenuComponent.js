f5d8de292483ae9a33e6c39fbc4433b98f3ccef5
Dina Bektemirova <helavissa@gmail.com>
2020-05-07 16:48:23 +0200

    single page applications part I

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,13 +4,14 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 	function RenderMenuItem({ dish, onClick }){
 		return(
-			<Card onClick={() => onClick(dish.id)}>
-							<CardImg width="100%" src={dish.image} alt={dish.name} />
-				
-							<CardImgOverlay>
-								<CardTitle>{dish.name}</CardTitle>
-							</CardImgOverlay>
-						</Card>
+			<Card 
+				onClick={() => onClick(dish.id)}>
+				<CardImg width="100%" src={dish.image} alt={dish.name} />
+	
+				<CardImgOverlay>
+					<CardTitle>{dish.name}</CardTitle>
+				</CardImgOverlay>
+			</Card>
 		);
 	}
 

ee576f97ea8b0de90a79233c76f0f5d21ab164d6
Dina Bektemirova <helavissa@gmail.com>
2020-05-28 23:18:31 +0200

    fetch post comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -25,7 +25,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 		);
 	}
 
-	function RenderComments({comments, addComment, dishId}){
+	function RenderComments({comments, postComment, dishId}){
 
 		if(comments == null){
 			return(
@@ -57,7 +57,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 						{dishComments}
 					</ul>
 					
-					<CommentForm dishId={dishId} addComment={addComment}/>	
+					<CommentForm dishId={dishId} postComment={postComment}/>	
 				</div>
 				);
 		}
@@ -84,7 +84,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 
 		handleSubmit(values){
 			this.toggleModal();
-			this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+			this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 	    }
 
 		render(){
@@ -207,7 +207,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 						<RenderDish dish={props.dish} />
 						<RenderComments 
 							comments={props.comments}
-							addComment={props.addComment}
+							postComment={props.postComment}
 							dishId={props.dish.id} />	
 					</div>
 				</div>

7c01f6bc9080db6c311da7f70307c730bd52e5f6
Dina Bektemirova <helavissa@gmail.com>
2020-05-29 23:58:23 +0200

    react animation components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -14,13 +15,19 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 	function RenderDish({dish}){
 		return(
 			<div className="col-12 col-md-5 m-1">
-				<Card>
-					<CardImg top src={baseUrl + dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle>{dish.name}</CardTitle>
-						<CardText>{dish.description}</CardText>
-					</CardBody>
-				</Card>
+				<FadeTransform
+                	in
+                	transformProps={{
+                    	exitTransform: 'scale(0.5) translateY(-50%)'
+                	}}>
+					<Card>
+						<CardImg top src={baseUrl + dish.image} alt={dish.name} />
+						<CardBody>
+							<CardTitle>{dish.name}</CardTitle>
+							<CardText>{dish.description}</CardText>
+						</CardBody>
+					</Card>
+				</FadeTransform>
 			</div>
 		);
 	}
@@ -32,29 +39,32 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 				<div></div>
 				);
 		}else{
-			const dishComments = comments.map((comment) => {
-				return (
-						<li key={comment.id}>
-							<div className="pb-2">
-								{comment.comment}
-								<br/>
-								--{comment.author},&nbsp; 
-								{new Intl.DateTimeFormat('en-US', {
-									year: 'numeric',
-									month: 'short',
-									day: '2-digit'
-								}).format(new Date(comment.date))}
-							</div>
-						</li>
-					);
-			});
 
 			return(
 				<div className="col-12 col-md-5 m-1">
 					
 					<h4>Comments</h4>
 					<ul className="list-unstyled">
-						{dishComments}
+						<Stagger in>
+						{comments.map((comment) => {
+							return (
+								<Fade in>
+									<li key={comment.id}>
+										<div className="pb-2">
+											{comment.comment}
+											<br/>
+											--{comment.author},&nbsp; 
+											{new Intl.DateTimeFormat('en-US', {
+												year: 'numeric',
+												month: 'short',
+												day: '2-digit'
+											}).format(new Date(comment.date))}
+										</div>
+									</li>
+								</Fade>
+								);
+						})}
+						</Stagger>
 					</ul>
 					
 					<CommentForm dishId={dishId} postComment={postComment}/>	
